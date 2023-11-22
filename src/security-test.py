312c80120930fc80ac5e4c799a13fdf48efd02a0
DamianMartins <martinsdaconceicao.damian@gmail.com>
2023-11-22 07:06:11 +0000

    test2

--- src/security-test.py
+++ src/security-test.py
@@ -2,7 +2,7 @@ import subprocess
 from bs4 import BeautifulSoup
 
 # Ruta al archivo index.html
-html_file_path = "../index.html"
+html_file_path = "/app/index.html"
 
 # Leer el contenido del archivo HTML
 with open(html_file_path, "r") as html_file:
@@ -10,32 +10,43 @@ with open(html_file_path, "r") as html_file:
 
 # Utilizar BeautifulSoup para extraer el contenido del script JavaScript
 soup = BeautifulSoup(html_content, "html.parser")
-script_content = soup.find("script").string
+script_tags = soup.find_all("script")
 
-# Guardar el contenido del script en un archivo temporal
-temp_script_path = "/tmp/temp_script.js"
-with open(temp_script_path, "w") as temp_script:
-    temp_script.write(script_content)
+# Verificar si se encontraron scripts en el HTML
+if script_tags:
+    script_content = "\n".join(tag.string for tag in script_tags if tag.string)
 
-# Ejecutar bandit en el archivo temporal
-try:
-    result = subprocess.run(["bandit", "-r", temp_script_path], check=True, capture_output=True, text=True)
-    output = result.stdout
+    # Verificar si script_content es una cadena antes de intentar escribirlo en el archivo temporal
+    if script_content and isinstance(script_content, str):
+        # Guardar el contenido del script en un archivo temporal
+        temp_script_path = "/tmp/temp_script.js"
+        with open(temp_script_path, "w") as temp_script:
+            temp_script.write(script_content)
 
-    # Verificar si se encontraron vulnerabilidades.
-    if "No issues identified" not in output:
-        print('Encontradas vulnerabilidades en el código JavaScript')
+        try:
+            # Ejecutar bandit en el archivo temporal
+            result = subprocess.run(["bandit", "-r", temp_script_path], check=True, capture_output=True, text=True)
+            output = result.stdout
 
-        # Imprimir la salida de bandit
-        print(output)
+            # Verificar si se encontraron vulnerabilidades.
+            if "No issues identified" not in output:
+                print('Encontradas vulnerabilidades en el código JavaScript')
+
+                # Imprimir la salida de bandit
+                print(output)
+            else:
+                print('No se encontraron vulnerabilidades en el código JavaScript')
+        except subprocess.CalledProcessError as e:
+            # Manejar errores de bandit
+            print(f'Error al ejecutar bandit: {e.stderr}')
+        except Exception as e:
+            # Manejar otros errores
+            print(f'Error: {e}')
+
+        # Eliminar el archivo temporal
+        subprocess.run(["rm", temp_script_path])
     else:
-        print('No se encontraron vulnerabilidades en el código JavaScript')
-except subprocess.CalledProcessError as e:
-    # Manejar errores de bandit
-    print(f'Error al ejecutar bandit: {e.stderr}')
-except Exception as e:
-    # Manejar otros errores
-    print(f'Error: {e}')
+        print('El contenido del script no es una cadena')
+else:
+    print('No se encontró un script JavaScript en el archivo HTML')
 
-# Eliminar el archivo temporal
-subprocess.run(["rm", temp_script_path])
